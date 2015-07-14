0f28b43c58c430f4c3158299792445a58b562a10
pradeeban <kk.pradeeban@gmail.com>
2015-07-14 09:00:31 +0200

    Removing unused constants.

--- modules/datacafe-server/src/main/java/edu/emory/bmi/datacafe/constants/HDFSConstants.java
+++ modules/datacafe-server/src/main/java/edu/emory/bmi/datacafe/constants/HDFSConstants.java
@@ -14,15 +14,10 @@ import java.io.File;
  * Constants for Hadoop/Hive Integration.
  */
 public class HDFSConstants {
-    public static final String HADOOP_HOME = "/home/pradeeban/programs/hadoop-2.7.0";
-    public static final String HADOOP_CONF = HADOOP_HOME + File.separator + "etc" + File.separator + "hadoop";
-    public static final String HDFS_PATH = "/user/hive/warehouse";
     public static String DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
     public static String HIVE_CONNECTION_URI = "jdbc:hive2://localhost:10000/default";
     public static String HIVE_USER_NAME = "pradeeban";
     public static String HIVE_PASSWORD = "";
 
     public static final String HIVE_CSV_DIR = "gsoc2015/conf/";
-    public static final String HIVE_CSV_FILE = "datawarehouse.csv";
-    public static final String HIVE_CSV_PATH = HIVE_CSV_DIR + HIVE_CSV_FILE;
 }
