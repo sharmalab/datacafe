d71f79a0d92d5d9a4f6f048a03fae5380d9c83a8
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2016-04-20 15:21:42 -0400

    Overwrite folder in hdfs.

--- modules/datacafe-server/src/main/java/edu/emory/bmi/datacafe/hdfs/HadoopConnector.java
+++ modules/datacafe-server/src/main/java/edu/emory/bmi/datacafe/hdfs/HadoopConnector.java
@@ -57,6 +57,20 @@ public class HadoopConnector {
         logger.info("Successfully written " + outputFileName + " to the Hadoop HDFS");
     }
 
+    private static FileSystem getFileSystem() throws IOException {
+        Configuration config = new Configuration();
+        config.addResource(new Path(ConfigReader.getHadoopConf()+ File.separator + HDFSConstants.CORE_SITE_XML));
+        config.addResource(new Path(ConfigReader.getHadoopConf() + File.separator + HDFSConstants.HDFS_SITE_XML));
+
+        return FileSystem.get(config);
+    }
+
+    private static void delete(String folder) throws IOException {
+        FileSystem fs = getFileSystem();
+        fs.delete(new Path(ConfigReader.getHdfsPath() + folder), true);
+        logger.info("Successfully deleted the contents of the HDFS folder: ", folder);
+    }
+
     /**
      * Writes the file to HDFS
      */
@@ -71,9 +85,10 @@ public class HadoopConnector {
     public static void main(String[] args) {
         ConfigReader.readConfig();
         try {
+            delete(ConfigReader.getInputBulkDir());
             copyToHDFS(ConfigReader.getInputBulkDir(), true);
         } catch (IOException e) {
-            logger.error("Exception in copying the directory" + ConfigReader.getInputBulkDir(), e);
+            logger.error("Exception in copying the directory: " + ConfigReader.getInputBulkDir(), e);
         }
     }
 }
