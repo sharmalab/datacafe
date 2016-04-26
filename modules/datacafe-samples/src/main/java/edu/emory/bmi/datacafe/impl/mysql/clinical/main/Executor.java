c7756d032149ee22c1a6033c5f36a7c3ab9ea21d
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2016-04-26 16:06:06 -0400

    Add MySql to the project.

--- modules/datacafe-samples/src/main/java/edu/emory/bmi/datacafe/impl/mysql/clinical/main/Executor.java
+++ modules/datacafe-samples/src/main/java/edu/emory/bmi/datacafe/impl/mysql/clinical/main/Executor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.emory.bmi.datacafe.impl.mongo.clinical.main;
+package edu.emory.bmi.datacafe.impl.mysql.clinical.main;
 
 import edu.emory.bmi.datacafe.core.CoreExecutorEngine;
 import edu.emory.bmi.datacafe.core.DataSourcesRegistry;
@@ -21,11 +21,10 @@ import edu.emory.bmi.datacafe.hdfs.HdfsConnector;
 import edu.emory.bmi.datacafe.mongo.MongoConnector;
 import org.bson.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A sample executor for Mongo, where the _id is considered as part of the original data.
+ * A sample executor for MySQL, where the _id is considered as part of the original data.
  */
 public class Executor {
     public static void main(String[] args) {
@@ -33,27 +32,27 @@ public class Executor {
         CoreExecutorEngine.init();
 
         String database1 = "clinical";
-        String collection1 = "clinicalData";
+        String table1 = "clinical";
 
         String database2 = "pathology";
-        String collection2 = "pathologyData";
+        String table2 = "pathology";
 
-        DataSourcesRegistry.addDataSource(database1, collection1);
-        DataSourcesRegistry.addDataSource(database2, collection2);
+        DataSourcesRegistry.addDataSource(database1, table1);
+        DataSourcesRegistry.addDataSource(database2, table2);
 
-//        // Get the list of IDs from the first data source
-//        Document document1 = new Document("Age_at_Initial_Diagnosis", new Document("$gt", 60)).append("Laterality", "Left");
-//        List ids1 = MongoConnector.getID(database1, collection1, document1);
-//
-//        // Get the list of IDs from the second data source
-//        Document document2 = new Document("Tumor_Nuclei_Percentage", new Document("$gt", 65));
-//        List ids2 = MongoConnector.getID(database2, collection2, document2);
-//
+        // Get the list of IDs from the first data source
+        Document document1 = new Document("Age_at_Initial_Diagnosis", new Document("$gt", 60)).append("Laterality", "Left");
+        List ids1 = MongoConnector.getID(database1, table1, document1);
+
+        // Get the list of IDs from the second data source
+        Document document2 = new Document("Tumor_Nuclei_Percentage", new Document("$gt", 65));
+        List ids2 = MongoConnector.getID(database2, table2, document2);
+
         // Interested Attributes: "patientID", "gender", "laterality"
-        List chosenAttributes1 = new ArrayList<>(); // = MongoConnector.getAttributeValues(database1, collection1, ids1, new String[]{"Gender", "Laterality"});
+        List chosenAttributes1 = MongoConnector.getAttributeValues(database1, table1, ids1, new String[]{"Gender", "Laterality"});
 
         // Interested Attributes: "sliceID", "patientID", "slideBarCode"
-        List chosenAttributes2 = new ArrayList<>(); // = MongoConnector.getAttributeValues(database2, collection2, ids2, new String[]{"BCR_Patient_UID_From_Pathology", "Slide_Barcode"});
+        List chosenAttributes2 = MongoConnector.getAttributeValues(database2, table2, ids2, new String[]{"BCR_Patient_UID_From_Pathology", "Slide_Barcode"});
 
         List<String>[] attributes = new List[]{chosenAttributes1, chosenAttributes2};
         String[] dataSourcesNames = DataSourcesRegistry.getFullNamesAsArray();
